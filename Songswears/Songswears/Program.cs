24e4842375c9f3dafc96ee975add05ff0fe2e2e6
filipkmiecik <fizzcrsnk500@gmail.com>
2019-05-22 14:41:01 +0200

    gitignore

---
+++ Songswears/Songswears/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Songswears
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var songAnalysis = new SongAnalysis("Kazik", "12 groszy");
+            Console.ReadLine();
+        }
+    }
+
+    class SongAnalysis
+    {
+        public SongAnalysis(string band, string song)
+        {
+            var browser = new WebClient();
+            var url = "https://api.lyrics.ovh/v1/"+band+"/"+song;
+            var json = browser.DownloadString(url);
+            Console.WriteLine(json);
+
+        }
+    }
+}
