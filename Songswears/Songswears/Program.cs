2a72048f0b88e6b9cda5d7be1eee84c977ba33e9
filipkmiecik <fizzcrsnk500@gmail.com>
2019-05-24 14:56:19 +0200

    end of lesson 24 05

--- Songswears/Songswears/Program.cs
+++ Songswears/Songswears/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Songswears
@@ -11,10 +14,39 @@ namespace Songswears
     {
         static void Main(string[] args)
         {
-            var songAnalysis = new SongAnalysis("Kazik", "12 groszy");
+            // var songAnalysis = new SongAnalysis("Kazik", "12 groszy");
+            var tekst = "oh shit waddup";
+            var censore = new Censore();
+            Console.WriteLine(censore.Fix(tekst));
             Console.ReadLine();
         }
-    }
+
+
+
+        class Censore
+    {
+        string[] badWords;
+
+        public Censore()
+        {
+            var profanitiesFile = File.ReadAllText("profanities.txt");
+            profanitiesFile = profanitiesFile.Replace("*", "");
+            profanitiesFile = profanitiesFile.Replace("(", "");
+            profanitiesFile = profanitiesFile.Replace(")", "");
+            profanitiesFile = profanitiesFile.Replace("\"", "");
+            badWords = profanitiesFile.Split(',');
+
+            public Fix(string tekst)
+            {
+                tekst = ReplaceBadWord(tekst);
+            }
+
+            private static string ReplaceBadWord(string tekst, string word)
+            {
+                var pattern = "\\b"+ReplaceBadWord+"\\b";
+                    return Regex.Replace(tekst, pattern, "__");
+            }
+        }
 
     class SongAnalysis
     {
