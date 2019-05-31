1e8c4e64b110d2eaf160e41389437bfb6b7f6b64
filipkmiecik <fizzcrsnk500@gmail.com>
2019-05-31 14:53:46 +0200

    lesson 31 05

--- Songswears/Songswears/Program.cs
+++ Songswears/Songswears/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -10,60 +9,60 @@ using System.Threading.Tasks;
 
 namespace Songswears
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
-            // var songAnalysis = new SongAnalysis("Kazik", "12 groszy");
-            var tekst = "oh shit waddup";
-            var censore = new Censore();
-            Console.WriteLine(censore.Fix(tekst));
+            var EminemSwearStats = new SwearStats();
+            var song = new Song("Eminem", "Stan");
+            EminemSwearStats.AddSwearsFrom(song);
+            //var censore = new Censore();
+            //Console.WriteLine(censore.Fix(song.lyrics));
             Console.ReadLine();
         }
 
-
-
-        class Censore
-    {
-        string[] badWords;
-
-        public Censore()
+             class SwearStats:Censore
         {
-            var profanitiesFile = File.ReadAllText("profanities.txt");
-            profanitiesFile = profanitiesFile.Replace("*", "");
-            profanitiesFile = profanitiesFile.Replace("(", "");
-            profanitiesFile = profanitiesFile.Replace(")", "");
-            profanitiesFile = profanitiesFile.Replace("\"", "");
-            badWords = profanitiesFile.Split(',');
+             Dictionary<string, int> allSwears = new Dictionary<string, int>();
+             public void AddSwearsFrom(Song song) {
+                foreach(var word in badWords)
+                {
+                    var countOccurences = song.CountOccurences(word);
+                }
 
-            public Fix(string tekst)
-            {
-                tekst = ReplaceBadWord(tekst);
-            }
-
-            private static string ReplaceBadWord(string tekst, string word)
-            {
-                var pattern = "\\b"+ReplaceBadWord+"\\b";
-                    return Regex.Replace(tekst, pattern, "__");
             }
         }
 
-    class SongAnalysis
-    {
-        public SongAnalysis(string band, string song)
+        class Song
         {
-            var browser = new WebClient();
-            var url = "https://api.lyrics.ovh/v1/"+band+"/"+song;
-            var json = browser.DownloadString(url);
-            var lyrics = JsonConvert.DeserializeObject<LyricsovhAnswer>(json);
-            Console.WriteLine(lyrics.lyrics);
+            public string title;
+            public string artist;
+            public string lyrics;
+            public Song(string band, string songName)
+            {
+                var browser = new WebClient();
+                var url = "https://api.lyrics.ovh/v1/" + band + "/" + songName;
+                var json = browser.DownloadString(url);
+                var lyricsData = JsonConvert.DeserializeObject<LyricsovhAnswer>(json);
 
+                title = songName;
+                artist = band;
+                lyrics = lyricsData.lyrics;
+
+            }
+
+            internal object CountOccurences(string word)
+            {
+                var pattern = "\\b" + word + "\\b";
+                return Regex.Matches(lyrics, pattern, RegexOptions.IgnoreCase).Count;
+            }
+        }
+
+        class LyricsovhAnswer
+        {
+            public string lyrics;
+            public string error;
         }
     }
 
-    class LyricsovhAnswer
-    {
-        public string lyrics;
-        public string error; 
-    }
 }
